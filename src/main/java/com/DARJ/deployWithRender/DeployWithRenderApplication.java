a75407921c690ac596cecd7aee203cde914ecc8d
DiegoAlejandroRJ <diego6382@gmail.com>
2024-04-15 22:42:10 -0500

    first commit

---
+++ src/main/java/com/DARJ/deployWithRender/DeployWithRenderApplication.java
@@ -0,0 +1,14 @@
+package com.DARJ.deployWithRender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class DeployWithRenderApplication{
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeployWithRenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DARJ/deployWithRender/DeployWithRenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DARJ.deployWithRender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployWithRenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
